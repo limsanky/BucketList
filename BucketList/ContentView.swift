b65684bdd5c9004ab796876911e3386c20dbd3c2
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-08 01:13:29 +0900

    update contentview

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,67 @@
 //  Created by Sankarshana V on 2022/02/07.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel: ViewModel
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                            .fixedSize()
+                    }
+                    .onTapGesture {
+                        viewModel.selectedPlace = location
+                    }
+                }
+            }
+            .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        viewModel.addLocation()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .sheet(item: $viewModel.selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                viewModel.update(location: newLocation)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: ContentView.ViewModel())
     }
 }
