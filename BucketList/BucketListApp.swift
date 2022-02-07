84dffbc39811e41a1e91b926fc01b3bb92879dbb
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-08 01:14:32 +0900

    Create and connect the ViewModel

--- BucketList/BucketListApp.swift
+++ BucketList/BucketListApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct BucketListApp: App {
+    @StateObject private var viewModel = ContentView.ViewModel()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: viewModel)
         }
     }
 }
