b9f34d3fa65e8e98f4a6f96897c655bd7f7b8026
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-07 19:14:06 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Sankarshana V on 2022/02/07.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
